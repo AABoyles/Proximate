8c2f8fac520a7065e28abaa1dfea7912958f16e2
Tony Boyles <Anthony@Boyles.cc>
2018-03-12 11:39:18 -0400

    Fixed a bunch of stuff to make it friendlier to IE
    I hate this so much.

--- main.js
+++ main.js
@@ -1,103 +1,115 @@
-var map, counties, showResult, findNeighbors, countyLayer, zoomToFeature, round;
-
 $(function(){
+  'use strict';
+
   $('#loading-warning').fadeIn();
 
-  map = new L.Map('map-wrapper', { center: new L.LatLng(39.828175, -98.5795), zoom: 5 });
-  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', { maxZoom: 18 }).addTo(map);
+  var map = new L.Map('map-wrapper', {
+    'center': new L.LatLng(39.828175, -98.5795),
+    'zoom': 5
+  });
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    'maxZoom': 18
+  }).addTo(map);
 
-  $.getJSON('counties.json', function(data){
-    counties = data;
+  var neighbors = [];
 
-    countyLayer = L.geoJSON(counties, {
-      onEachFeature: function(feature, layer){
+  $.getJSON('counties.json', function(counties){
+
+    var countyLayer = L.geoJSON(counties, {
+      'onEachFeature': function(feature, layer){
         layer.on({
-          click: function(e){
+          'click': function(e){
             zoomToFeature(e.target.feature);
             showResult(e.target.feature);
           }
         });
       }
-    })
-    .bindTooltip(function(layer){return layer.feature.properties.NAME + ', ' + layer.feature.properties.USPS;}, {sticky: 1})
-    .addTo(map);
+    }).bindTooltip(function(layer){
+        return layer.feature.properties.NAME + ', ' + layer.feature.properties.USPS;
+      }, {
+        sticky: 1
+    }).addTo(map);
 
-    new Awesomplete($('#search')[0], {
-      list: counties.features.map(function(c){return c.properties.NAME + ', ' + c.properties.USPS;})
+    new Awesomplete($('#search').get(0), {
+      'list': counties.features.map(
+        function(c){
+          return c.properties.NAME + ', ' + c.properties.USPS;
+        })
     });
 
     $('#search').on('awesomplete-selectcomplete', function(){
-      var county, state;
-      [county, state] = this.value.split(', ');
-      var feature = counties.features.find(function(c){return c.properties.NAME == county && c.properties.USPS == state});
-      zoomToFeature(feature);
-      showResult(feature);
-      $('#loading-cover').fadeOut();
+      var feature;
+      for(var i = counties.features.length-1; i >= 0; i--){
+        if(this.value === counties.features[i].properties.NAME + ', ' + counties.features[i].properties.USPS){
+          feature = counties.features[i];
+          break;
+        }
+      }
+      if(feature){
+        zoomToFeature(feature);
+        showResult(feature);
+      }
     });
 
-    zoomToFeature = function(feature){
+    function zoomToFeature(feature){
       var ll = L.latLng(feature.properties.LAT, feature.properties.LON);
       map.setView(ll, 8);
       L.popup()
         .setLatLng(ll)
         .setContent(feature.properties.NAME + ', ' + feature.properties.USPS)
         .openOn(map);
-    };
+    }
 
-    showResult = function(feature){
-      $('#neighbor-stats').slideUp(function(){
-        var neighbors = counties.features.filter(function(c){return feature.properties.neighbors.includes(c.properties.STATE + c.properties.COUNTY);});
+    function showResult(feature){
+      $('#neighbor-stats').fadeOut(function(){
+        neighbors = counties.features.filter(function(c){
+          return feature.properties.neighbors.indexOf(c.properties.STATE + c.properties.COUNTY) !== -1;
+        });
         $(this).find('tbody').html(
           '<tr style="font-style:italic">' +
-            ['NAME', 'USPS'].map(function(v){return '<td>' + feature.properties[v] + '</td>'}).join('') +
-            ['POPULATION', 'CENSUSAREA'].map(function(v){return '<td>' + feature.properties[v].toLocaleString() + '</td>' }).join('') +
+            ['NAME', 'USPS'].map(function(v){
+              return '<td>' + feature.properties[v] + '</td>';
+            }).join('') +
+            ['POPULATION', 'CENSUSAREA'].map(function(v){
+              return '<td>' + feature.properties[v].toLocaleString() + '</td>';
+            }).join('') +
             '<td>' + round(feature.properties.POPULATION/feature.properties.CENSUSAREA, 1) + '</td>' +
           '</tr>' +
           neighbors.map(function(n){
             return '<tr>' +
-              ['NAME', 'USPS'].map(function(v){return '<td>' + n.properties[v] + '</td>'}).join('') +
-              ['POPULATION', 'CENSUSAREA'].map(function(v){return '<td>' + n.properties[v].toLocaleString() + '</td>'}).join('') +
+              ['NAME', 'USPS'].map(function(v){
+                return '<td>' + n.properties[v] + '</td>';
+              }).join('') +
+              ['POPULATION', 'CENSUSAREA'].map(function(v){
+                return '<td>' + n.properties[v].toLocaleString() + '</td>';
+              }).join('') +
               '<td>' + round(n.properties.POPULATION/n.properties.CENSUSAREA, 1) + '</td>' +
             '</tr>';
           }).join('\n')
         );
-        $(this).slideDown();
+        $(this).fadeIn();
       });
-      $('#export').attr('download', 'Neighbors_of_' + feature.properties.NAME.replace(/ /g, '_') + '_' + feature.properties.USPS + '.csv');
-    };
-
+    }
 
   }).done(function(e){
     $('#loading-cover').fadeOut();
   });
 
-  $('#export').on('mousedown', function(e){
+  $('#export').click(function(e){
     var headers = 'NAME,STATE,POPULATION,AREA(SQMI),DENSITY';
-    var content = $('#neighbors tr').map(function(){
-      return $(this).find('td').map(function(){
-        return $(this).text().replace(/,/g,'')
-      }).get().join(',')
-    }).get().join('\r\n');
-    $(this).attr('href', 'data:text/csv;charset=utf-8,' + headers + '\n' + content);
+    var content = neighbors.map(function(county){
+      return ['NAME', 'USPS', 'POPULATION', 'CENSUSAREA'].map(function(field){
+        return (county.properties[field] + '').replace(/,/g,'');
+      }).join(',') + ',' + county.properties.POPULATION/county.properties.CENSUSAREA;
+    }).join('\r\n');
+    var blob = new Blob([headers + '\r\n' + content], {type: 'data:text/csv;charset=utf-8'});
+    saveAs(blob, 'adjacentCounties.csv');
   });
 
-  round = function(number, precision) {
+  function round(number, precision) {
     var factor = Math.pow(10, precision);
     var tempNumber = number * factor;
     var roundedTempNumber = Math.round(tempNumber);
     return roundedTempNumber / factor;
-  };
-
+  }
 });
-
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') start = 0;
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
-  };
-}
